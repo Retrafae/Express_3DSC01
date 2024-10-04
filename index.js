8c48816be4a187172dc98c9f2a641eaa15cd33a3
Art <130366200+Retrafae@users.noreply.github.com>
2024-10-04 14:59:24 +0000

    Criadas as rotas home e about.Iniciado o menu

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const PORT = 3300;
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/src/views/index.html");
+  //console.log(__dirname + "/src/views/index.html");
+});
+app.get("/about", function (req, res) {
   res.sendFile(__dirname + "/src/views/about.html");
   //console.log(__dirname + "/src/views/index.html");
+});
+app.use(function (req, res) {
+  res.status(404).sendFile(__dirname + "/src/views/404.html");
 });
 app.listen(PORT, () => {
   console.log("running...");
