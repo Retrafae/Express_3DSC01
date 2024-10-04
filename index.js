21bcf27e21ff671e8d4f75a46062baed86e26812
Art <130366200+Retrafae@users.noreply.github.com>
2024-10-04 14:12:37 +0000

    W2

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const PORT = 3300;
 
 app.get("/", function (req, res) {
-  res.send("Olá!");
+  res.sendFile(__dirname + "/src/views/index.html");
+  res.sendFile(__dirname + "/src/views/about.html");
+  //console.log(__dirname + "/src/views/index.html");
 });
 app.listen(PORT, () => {
   console.log("running...");
